612d6720d406a458e9eb060d8a14fa7491f4a4ce
ViolettWu5 <72916641+ViolettWu5@users.noreply.github.com>
2021-03-07 23:46:32 +0800

    Update py maxproduct.py

--- py/py maxproduct.py
+++ py/py maxproduct.py
@@ -1,13 +1,17 @@
-def maxProduct(nums):
-    cntX = 0
-    for i in nums:
-        for j in nums:
-            if(i == j):
-                continue
-            if(cntX < i*j):
-                cntX = i*j
-    return cntX
+ maxProduct(nums):
+    cntx = 0
+    if len(nums) == 2:
+        cntx = nums[0] * nums[1]
+    else:
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                sum = nums[i] * nums[j]
+                if sum > cntx:
+                    cntx = sum
+    return cntx
 
 
 print(maxProduct([5, 20, 2, 6]))
 print(maxProduct([10, -20, 0, 3]))
+print(maxProduct([1,-10]))
+
